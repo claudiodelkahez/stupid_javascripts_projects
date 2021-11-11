ae785dea1cbdf6a2feb396be5ae67ca7c17ef85b
elperrodev <elperrodev@gmail.com>
2021-11-11 14:05:22 -0300

    panarrabits game listo

---
+++ main.js
@@ -0,0 +1,24 @@
+let titi = panarrabits
+
+
+function () {
+
+    if titi = panarrabits 
+    
+    alert(`Eres ${parrabits} lina!`);
+
+    else 
+
+    alert(`No sos ${parrabits} culera!`);
+
+}
+
+function showMessage() {
+    alert( '¡Hola a todos!' );
+  }
+
+  let titi = prompt('¿Por casualidad sos titi?', '');
+
+    if (titi == si) alert( '¡Sos un panarrabits!' );
+
+
